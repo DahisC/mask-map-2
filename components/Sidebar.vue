79c05bdd30f915743a677c8b6571e2c904a26b24
DahisC <dahischeng@gmail.com>
2020-04-24 01:28:39 +0800

    added scroll to top feature

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -15,8 +15,8 @@
           >
         </div>
       </template>
-      <b-container fluid>
-        <b-row>
+      <b-container fluid :style="{ position: 'relative' }">
+        <b-row v-b-visible="visibleHandler">
           <b-col cols="5">
             <b-img src="~/assets/doctor-icon.svg" fluid />
           </b-col>
@@ -29,6 +29,9 @@
           <PharmacyCards />
         </b-row>
       </b-container>
+      <div id="scroll-up-btn" v-if="!isTopVisible" @click="scrollToTop">
+        <b-icon-arrow-bar-up variant="white" scale="1.5" />
+      </div>
     </b-sidebar>
   </div>
 </template>
@@ -37,13 +40,72 @@
 import ActivityDescription from "~/components/ActivityDescription";
 import PharmacyCards from "~/components/PharmacyCards";
 import Searching from "~/components/Searching";
+import { BIcon, BIconArrowBarUp } from "bootstrap-vue";
 
 export default {
-  components: { ActivityDescription, PharmacyCards, Searching }
+  data() {
+    return {
+      isTopVisible: true
+    };
+  },
+  components: {
+    ActivityDescription,
+    PharmacyCards,
+    Searching,
+    BIconArrowBarUp
+  },
+  methods: {
+    visibleHandler(isVisible) {
+      this.isTopVisible = isVisible;
+    },
+    scrollToTop() {
+      let target = document.querySelector(".b-sidebar-body");
+      target.scrollTop = 0;
+    }
+  }
 };
 </script>
 
 <style scoped>
+#scroll-up-btn {
+  position: absolute;
+  background-color: #0ba29c;
+  opacity: 0.75;
+  border-radius: 50%;
+  right: 0;
+  bottom: 0;
+  margin: 0px 20px 5px 0px;
+  height: 40px;
+  width: 40px;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+}
+
+#scroll-up-btn {
+  animation-name: floating;
+  animation-duration: 1s;
+  animation-iteration-count: infinite;
+  animation-timing-function: cubic-bezier(0, 0, 0, 0);
+}
+
+#scroll-up-btn:hover {
+  opacity: 1;
+}
+
+@keyframes floating {
+  0% {
+    bottom: 0px;
+  }
+  50% {
+    bottom: 10px;
+  }
+  100% {
+    bottom: 0px;
+  }
+}
+
 #sidebar > header > strong > span:nth-child(2) {
   font-size: 0.5em;
   color: rgba(0, 0, 0, 0.25);
